3b92cfc86b46eb08ef541f98dab4b2c1d3351f04
Jeremiah Damiron <jeremiahdamiron@gmail.com>
2021-08-31 11:05:23 -0400

    web3

--- src/store/interactions.js
+++ src/store/interactions.js
@@ -26,43 +26,33 @@ import Exchange from '../abis/Exchange.json'
 import { ETHER_ADDRESS } from '../helpers'
 
 export const loadWeb3 = async (dispatch) => {
-  if(typeof window.ethereum!=='undefined'){
-    const web3 = new Web3(window.ethereum)
-    dispatch(web3Loaded(web3))
-    return web3
-  } else {
-    window.alert('Please install MetaMask')
-    window.location.assign("https://metamask.io/")
+  let web3
+    if(typeof window.ethereum !== 'undefined') {
+      web3 = new Web3(window.ethereum)
+      await window.ethereum.request({ method: 'eth_requestAccounts' })
+      dispatch(web3Loaded(web3))
+      return web3
+    }
+    else if (window.web3) {
+      web3 = new Web3(window.web3.currentProvider)
+      dispatch(web3Loaded(web3))
+      return web3
+    }
+    else {
+      window.alert('Please install MetaMask')
+      window.location.assign("https://metamask.io/")
+    }
   }
-}
 
-export const loadAccount = async (web3, dispatch) => {
-  const accounts = await web3.eth.getAccounts()
-  const account = accounts[0]
-  dispatch(web3AccountLoaded(account))
-  return account
-}
-
-export const loadToken = async (web3, networkId, dispatch) => {
-  try {
-    const token = new web3.eth.Contract(Token.abi, Token.networks[networkId].address)
-    dispatch(tokenLoaded(token))
-    return token
-  } catch (error) {
-    console.log('Contract not deployed to the current network. Please select another network with Metamask.')
-    return null
-  }
-}
-
-export const loadExchange = async (web3, networkId, dispatch) => {
-  try {
-    const exchange = new web3.eth.Contract(Exchange.abi, Exchange.networks[networkId].address)
-    dispatch(exchangeLoaded(exchange))
-    return exchange
-  } catch (error) {
-    console.log('Contract not deployed to the current network. Please select another network with Metamask.')
-    return null
-  }
+  export const loadExchange = async (web3, networkId, dispatch) => {
+    try {
+      const exchange = new web3.eth.Contract(Exchange.abi, Exchange.networks[networkId].address)
+      dispatch(exchangeLoaded(exchange))
+      return exchange
+    } catch (error) {
+      console.log('Contract not deployed to the current network. Please select another network with Metamask.')
+      return null
+    }
 }
 
 export const loadAllOrders = async (exchange, dispatch) => {
